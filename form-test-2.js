f3eb8e238ea68de77978b4ee0baf40083444feb3
al1 <alex.tsumibito@gmail.com>
2023-08-03 10:19:19 +0200

    3

--- form-test-2.js
+++ form-test-2.js
@@ -1,6 +1,37 @@
-console.log('from test 2');
-console.log('from test 22');
-console.log('from test 222');
-console.log('from test 2222');
-console.log('from test 22222');
-console.log('from test 222222');
+const nameInput = $('#name');
+
+const ipInfoResp = function() {
+    $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+        return resp;
+    });
+};
+
+
+
+$(document).ready(function () {
+
+    console.log(ipInfoResp.ip);
+
+    nameInput.on('change blur', function() {
+        const value = nameInput.val();
+        console.log(nameInput.val());
+
+
+        if (value === "") {
+            nameInput.removeClass("success");
+            nameInput.removeClass("error");
+        } else if (/^[a-zA-Z\W]+$/.test(value)) {
+            nameInput.addClass("success");
+            nameInput.removeClass("error");
+        } else if (/^[0-9]+$/.test(value)) {
+            nameInput.addClass("error");
+            nameInput.removeClass("success");
+        } else {
+            nameInput.addClass("success");
+            nameInput.removeClass("error");
+        }
+    });
+
+
+
+});
