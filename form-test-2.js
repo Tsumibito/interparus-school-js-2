d43a9418c3e89647e3913d4786e4a1214642a21f
al1 <alex.tsumibito@gmail.com>
2023-08-07 18:33:41 +0200

    3

--- form-test-2.js
+++ form-test-2.js
@@ -3,20 +3,21 @@ const nameInput = $('[ip-name]');
 console.log('ipInfoResp.ip');
 console.log('ipInfoResp.ip-2223');
 
-const ipInfoResp = $.getJSON('https://ipapi.co/json/', function(data){
-    console.log(data)
-})
+const ipInfoResp = $.getJSON( "https://ipapi.co/json/", function( json ) {
+    console.log( "JSON Data: " + json.ip[ 1 ] );
+});
+
 
 
 $(document).ready(function () {
 
     console.log(ipInfoResp);
-    console.log(ipInfoResp[0].ip);
+
 
     nameInput.on('change blur', function() {
         const value = nameInput.val();
         console.log(nameInput.val());
-        console.log(ipInfoResp[0].ip);
+
 
         if (value === "") {
             nameInput.removeClass("success");
