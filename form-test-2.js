a6e62ef3afedb48b739dc9254a5e0258bc54d9ec
al1 <alex.tsumibito@gmail.com>
2023-08-14 17:15:18 +0200

    3

--- form-test-2.js
+++ form-test-2.js
@@ -8,34 +8,39 @@ const ipInfoResp = $.getJSON( "https://ipapi.co/json/", function( json ) {
     return json
 });
 
-$('#phone').intlTelInput ({
-    autoHideDialCode: true,
-    separateDialCode: true,
-    nationalMode: true,
-    initialCountry: "auto",
-    geoIpLookup: ipInfoResp.country_code,
-});
 
-/*
+
+
 $(document).ready(function () {
-
-    nameInput.on('change blur', function() {
-        const value = nameInput.val();
-        console.log(nameInput.val());
-
-        if (value === "") {
-            nameInput.removeClass("success");
-            nameInput.removeClass("error");
-        } else if (/^[a-zA-Z\W]+$/.test(value)) {
-            nameInput.addClass("success");
-            nameInput.removeClass("error");
-        } else if (/^[0-9]+$/.test(value)) {
-            nameInput.addClass("error");
-            nameInput.removeClass("success");
-        } else {
-            nameInput.addClass("success");
-            nameInput.removeClass("error");
-        }
+    $('#phone').intlTelInput ({
+        autoHideDialCode: true,
+        separateDialCode: true,
+        nationalMode: true,
+        initialCountry: "auto",
+        geoIpLookup: ipInfoResp.country_code,
     });
+
+
+    /*
+        nameInput.on('change blur', function() {
+            const value = nameInput.val();
+            console.log(nameInput.val());
+
+            if (value === "") {
+                nameInput.removeClass("success");
+                nameInput.removeClass("error");
+            } else if (/^[a-zA-Z\W]+$/.test(value)) {
+                nameInput.addClass("success");
+                nameInput.removeClass("error");
+            } else if (/^[0-9]+$/.test(value)) {
+                nameInput.addClass("error");
+                nameInput.removeClass("success");
+            } else {
+                nameInput.addClass("success");
+                nameInput.removeClass("error");
+            }
+        });
+
+ */
 });
-*/
+
