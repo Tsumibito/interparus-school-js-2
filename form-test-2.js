63c3a5003e60c12d61b9e0d4b61332ab334dd24a
al1 <alex.tsumibito@gmail.com>
2023-08-15 11:01:58 +0200

    3

--- form-test-2.js
+++ form-test-2.js
@@ -13,9 +13,10 @@ $(document).ready(function () {
         separateDialCode: true,
         nationalMode: true,
         initialCountry: "auto",
-        geoIpLookup: ipInfoResp.country_code,
+        geoIpLookup: 'us',//ipInfoResp.country_code,
         utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.1.1/build/js/utils.min.js",
     });
+
 
 
     /*
