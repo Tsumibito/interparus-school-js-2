82b4d230f3e810154b4684b975ba1d0977d0230c
al1 <alex.tsumibito@gmail.com>
2023-08-18 11:11:20 +0200

    3

--- form-test-2.js
+++ form-test-2.js
@@ -41,9 +41,42 @@ let ipInfo,
     EmailValidationResult = false,
     PhoneValidationResult = false;
 
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
 function UrlParams(pageUrl, paramName) {
     const params = new URLSearchParams(pageUrl.split('?')[1]);
-    return params.has(paramName) ? params.get(paramName) : 'None';
+
+    if (params.has(paramName)) {
+        const paramValue = params.get(paramName);
+        setCookie(paramName, paramValue, 1); // Устанавливаем куки на 24 часа
+        return paramValue;
+    } else {
+        const cookieValue = getCookie(paramName);
+        return cookieValue ? cookieValue : 'None';
+    }
 }
 
 function GetIpInfo(ipInfo, paramName) {
